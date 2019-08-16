de17ddf4f0faa0401fbc5176e715c298d955b1aa
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-08-15 21:00:27 -0300

    Repetição Controlada While

---
+++ RepeticaoControladaWhile/src/WhileCounter.java
@@ -0,0 +1,14 @@
+
+public class WhileCounter {
+
+	public static void main(String[] args) {
+		int controle = 1;
+		
+		while (controle <= 10) {
+			System.out.println(""+controle);			
+			++controle;
+		}
+		System.out.println();
+	}
+
+}
