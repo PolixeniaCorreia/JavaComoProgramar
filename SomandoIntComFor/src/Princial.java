ca6ac2885947882ef19af4ef3ec19a6c9fcdfaa5
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-08-23 21:29:34 -0300

    Usando For

---
+++ SomandoIntComFor/src/Princial.java
@@ -0,0 +1,13 @@
+
+public class Princial {
+
+	public static void main(String[] args) {
+	
+		int total = 0; 
+		for (int num = 2; num <= 20; num += 2)
+			total += num;
+		System.out.println(" soma  é: " + total);
+
+	}
+
+}
