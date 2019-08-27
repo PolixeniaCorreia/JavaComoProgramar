532135437a0c4c35b879c6b60a21f4c8fd2869e4
Polixenia Correia <polixeniacorreia@gmail.com>
2019-08-27 15:21:16 -0300

    Do While

---
+++ DoWhile/src/Principal.java
@@ -0,0 +1,15 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int conta = 1;
+		do {
+			System.out.println(" "+conta);
+			++conta;
+		} while (conta <= 10);
+			System.out.println();
+		
+
+	}
+
+}
