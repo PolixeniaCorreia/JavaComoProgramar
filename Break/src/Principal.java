359ee3ed7c27ed07c1d8be02f469cdeb8e7e4dee
Polixenia Correia <polixeniacorreia@gmail.com>
2019-08-29 14:21:39 -0300

    Break

---
+++ Break/src/Principal.java
@@ -0,0 +1,19 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int cont;
+		
+		for (cont = 1; cont <= 10; cont++) {
+			
+			if (cont == 5) 
+				
+			break;
+			System.out.println(" " + cont);
+			
+		}
+		System.out.println("Quebrou o loop na contagem: " + cont);	
+
+	}
+
+}
