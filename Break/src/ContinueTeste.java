e32d64f94e6339e564b948130126c69e1759e7de
Polixenia Correia <polixeniacorreia@gmail.com>
2019-08-29 15:19:06 -0300

    Usando o Continue

---
+++ Break/src/ContinueTeste.java
@@ -0,0 +1,18 @@
+
+public class ContinueTeste {
+
+	public static void main(String[] args) {
+		
+		for (int cont = 1; cont <= 10; cont++) {
+			
+			if (cont == 5) 
+			continue;	
+			
+			System.out.println(" " + cont);
+			
+		}
+		System.out.println("Usado continuar para pular a impressão");
+	}
+}
+
+
