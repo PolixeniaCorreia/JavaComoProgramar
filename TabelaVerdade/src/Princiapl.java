f23badfd142ac743788392eb338f33f21f85e99c
Polixenia Correia <polixeniacorreia@gmail.com>
2019-08-29 16:06:57 -0300

    Tabela Verdade

---
+++ TabelaVerdade/src/Princiapl.java
@@ -0,0 +1,51 @@
+
+public class Princiapl {
+
+	public static void main(String[] args) {
+		System.out.println("Condicional AND (&&) \n"+
+				"false && false: "+ (false && false)+ "\n"+
+				"false && true: " + (false && true)+ "\n"+
+				"true && false: " + (true && false)+ "\n"+
+				"true && true: " + (true && true)); 
+		
+		System.out.println("");
+		
+		System.out.println("Condicional OR (||) \n"+
+				"false || false: "+ (false || false)+ "\n"+
+				"false || true: " + (false || true)+ "\n"+
+				"true || false: " + (true || false)+ "\n"+
+				"true || true: " + (true || true));
+		
+		System.out.println("");
+		
+		System.out.println("Boolean logical AND (&) \n"+
+				"false & false: "+ (false & false)+ "\n"+
+				"false & true: " + (false & true)+ "\n"+
+				"true & false: " + (true & false)+ "\n"+
+				"true & true: " + (true & true));
+		
+		System.out.println("");
+		
+		System.out.println("Boolean logical inclusive OR (|) \n"+
+				"false | false: "+ (false | false)+ "\n"+
+				"false | true: " + (false | true)+ "\n"+
+				"true | false: " + (true | false)+ "\n"+
+				"true | true: " + (true | true));
+		
+		System.out.println("");
+		
+		System.out.println("Boolean logical exclusive OR (^) \n"+
+				"false ^ false: "+ (false ^ false)+ "\n"+
+				"false ^ true: " + (false ^ true)+ "\n"+
+				"true ^ false: " + (true ^ false)+ "\n"+
+				"true ^ true: " + (true ^ true));
+		
+		System.out.println("");
+		
+		System.out.println("Logical NOT (!) \n"+
+				"!false: " + (!false)+ "\n"+
+				"!true: " + (!true));
+		
+	}
+
+}
