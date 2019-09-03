631e35c3fcd26aad6e8feec4fb95c3d02023a9cf
Polixenia Correia <polixeniacorreia@gmail.com>
2019-09-03 16:51:46 -0300

    Array

---
+++ Array/src/Principal.java
@@ -0,0 +1,14 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int [] array = new int [10];
+		
+		System.out.println(" " + " Index " + " Value ");
+
+		for(int contador = 0; contador < array.length; contador++) {
+			System.out.println("   " + contador + "        " +   array[contador]);
+		}
+	}
+
+}
