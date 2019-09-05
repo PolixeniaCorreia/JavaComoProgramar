80e623cde7051f1320465d0e15093982e4ce1560
PolixeniaCorreia <polixeniacorreia@gmail.com>
2019-09-05 20:04:32 -0300

    Array - Imprimindo Barras

---
+++ ArrayImpressaoBarra/src/Principal.java
@@ -0,0 +1,26 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int[] array = { 0, 0, 0, 0, 0, 0, 1, 2, 4, 2, 1 };
+		System.out.println("Distribuição de notas:");
+	
+		for (int cont = 0; cont < array.length; cont++){
+		
+			if (cont == 10)
+			System.out.printf("  "+100+":");
+			
+			else
+			System.out.printf("%02d-%02d: ",
+					cont * 10, cont * 10 + 9);
+	
+		
+			for (int stars = 0; stars < array[cont]; stars++)
+			System.out.print("*");
+		
+			System.out.println();
+		}
+	
+	}
+
+}
